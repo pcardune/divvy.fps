87b0df9299da564cd0d99f0b469dea46820653b6
Paul Carduner <paul@carduner.net>
2009-10-20 10:22:04 -0700

    Add license information.

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+########################################################################
+# Copyright (c) 2009 Paul Carduner and Contributors
+# All Rights Reserved
+# This file is part of divvy.fps.
+#
+# divvy.fps is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# divvy.fps is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with divvy.fps.  If not, see
+# <http://www.gnu.org/licenses/>.
+#########################################################################
+
 import os
 from setuptools import setup, find_packages
 
@@ -13,8 +33,10 @@ setup(
         read('README.txt')
         + '\n\n' +
         read('CHANGES.txt')
+        + '\n\n' +
+        read('AUTHORS.txt')
         ),
-    license = "proprietary",
+    license = "GNU LGPL v3",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     namespace_packages = ['divvy'],
--- src/divvy/fps/conf.py
+++ src/divvy/fps/conf.py
@@ -1,3 +1,22 @@
+########################################################################
+# Copyright (c) 2009 Paul Carduner and Contributors
+# All Rights Reserved
+# This file is part of divvy.fps.
+#
+# divvy.fps is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# divvy.fps is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with divvy.fps.  If not, see
+# <http://www.gnu.org/licenses/>.
+#########################################################################
 
 try:
     from django.conf import settings
